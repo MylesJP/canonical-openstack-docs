07449206a36b5d1a3226ba5c5c1a2bf103e1db8a
Myles Penner <myles.penner@canonical.com>
2025-10-14 15:41:37 -0700

    Add CAAS feature to testing

--- how-to/snippets/enable-dummy-feature.task.sh
+++
@@ -1,7 +0,0 @@
-# @depends tutorial/snippets/get-started-with-openstack.task.sh
-
-# [docs-exec:enable-feature-x]
-echo "Hello from dummy feature"
-# [docs-exec:enable-feature-x-end]
-
-# Some change 4
