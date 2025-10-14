a7e9e30e6a004e95b27f7141609bbca0d0afc61f
Myles Penner <myles.penner@canonical.com>
2025-10-14 15:10:26 -0700

    Change how-to

--- how-to/snippets/enable-dummy-feature.task.sh
+++ how-to/snippets/enable-dummy-feature.task.sh
@@ -1,5 +1,7 @@
 # @depends tutorial/snippets/get-started-with-openstack.task.sh
 
 # [docs-exec:enable-feature-x]
-sg snap_daemon "sunbeam enable feature-x"
+echo "Hello from dummy feature"
 # [docs-exec:enable-feature-x-end]
+
+# Some change
